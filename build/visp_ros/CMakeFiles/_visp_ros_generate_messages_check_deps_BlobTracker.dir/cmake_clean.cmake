78b39a851148e0e02e3bc1a923d37c535f93f3f6
Erandi Ireri <163090631+erandivg@users.noreply.github.com>
2025-03-19 18:05:18 -0600

    Delete build directory

--- build/visp_ros/CMakeFiles/_visp_ros_generate_messages_check_deps_BlobTracker.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/_visp_ros_generate_messages_check_deps_BlobTracker"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/_visp_ros_generate_messages_check_deps_BlobTracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/visp_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/erandi/Documents/ros/strawberry_ws/src/visp_ros
-# Build directory: /home/erandi/Documents/ros/strawberry_ws/build/visp_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
